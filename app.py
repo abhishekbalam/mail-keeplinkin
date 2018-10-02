cbaf1cd3821fc84d77fbe3293746daa04f9c262f
abhishekbalam <abhishekbalam@gmail.com>
2018-10-02 18:36:11 +0530

    Inital Commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, render_template, request, redirect, jsonify, abort
+import requests
+from flask_mail import Mail, Message
+import db
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER']='smtp.zoho.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.environ['zoho_username']
+app.config['MAIL_PASSWORD'] = os.environ['zoho_password']
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+@app.route('/')	
+def main():
+	return render_template('index.html')
+
+@app.route('/sent/')	
+def sent():
+	return render_template('sent.html')	
+
+def verify(email):
+	token="7996de2b-d743-4536-a107-5252fec5c828"
+	url="https://api.trumail.io/v2/lookups/json?email="+email+"&token="+token
+	print(url)
+	data=requests.get(url).json()
+	return data['deliverable']
+
+@app.route('/send', methods = ['POST'])	
+def send():
+	data=request.form
+	
+	email=data['email'].strip()
+	subject=data['subject'].strip()
+	link=data['links'].strip()
+	
+	print(link)
+	
+	# abort(404)
+
+	if(verify(email)==False):
+		text="Invalid Email"
+		return render_template('error.html', text=text, again=True)
+
+	mail = Mail(app)
+	msg = Message('Confirm Your Email', sender = ('Abhishek Balam', 'newsletter@abhishekbalam.xyz'), recipients = [email])
+	msg.html = render_template('confirm.html', name=subject, email=email) 
+	test=mail.send(msg)
+	
+	print(test)
+	return redirect("/sent/", code=302)
+
+if __name__ == '__main__':
+	app.jinja_env.auto_reload = True
+	app.config['TEMPLATES_AUTO_RELOAD'] = True
+	# app.run(debug=True, port=4000)
+	app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,41 @@
+import os
+import redis
+
+# For Remote DB
+# db=redis.from_url(os.environ['REDISCLOUD_URL'])
+
+# For Local DB
+db=redis.Redis(host='localhost', port=6379, password='')
+
+def adduser(firstname,lastname,email):
+	name=firstname+','+lastname+',0'
+	if(db.hsetnx("users",email,name)):
+		return True
+	else:
+		return False
+
+def getusers():
+	return db.hgetall("users");
+
+def validateuser(email):
+	user=db.hget("users", email);	
+	if(user is None ):
+		return False
+	else:
+		user=user.replace('0','1')
+		db.hset("users", email, user)
+		return True
+
+def deluser(email):
+	status=db.hdel("users",email)
+	print(email)
+	print(status)
+	if(status):
+		return True
+	else:
+		return False
+
+
+# adduser("Abhishek","Balam","abhishekbalam@gmail.com")
+# adduser("Rambo","Balam","abhishekbalam96@gmail.com")
+# adduser("Arnold","Balam","czar2302@gmail.com")
